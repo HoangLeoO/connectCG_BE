29942d7b5bc2a1993dd3b27c8df694077b5bba6e
HoangLeoO <leoo09012001@gmail.com>
2026-01-29 11:05:11 +0700

    oke

--- src/main/java/org/example/connectcg_be/service/impl/AuthServiceImpl.java
+++ src/main/java/org/example/connectcg_be/service/impl/AuthServiceImpl.java
@@ -25,8 +25,7 @@ public class AuthServiceImpl implements AuthService {
     private UserRepository userRepository;
     @Autowired
     private UserProfileRepository userProfileRepository;
-    @Autowired
-    private CityRepository cityRepository;
+
     @Autowired
     private MediaRepository mediaRepository; // [NEW]
     @Autowired
@@ -128,11 +127,11 @@ public class AuthServiceImpl implements AuthService {
             } catch (Exception e) {
             }
         }
-        if (request.getCityId() != null) {
-            try {
-                cityRepository.findById(Integer.parseInt(request.getCityId())).ifPresent(profile::setCity);
-            } catch (Exception e) {
-            }
+        if (request.getCityCode() != null) {
+            profile.setCityCode(request.getCityCode());
+        }
+        if (request.getCityName() != null) {
+            profile.setCityName(request.getCityName());
         }
 
         UserProfile savedProfile = userProfileRepository.save(profile);
