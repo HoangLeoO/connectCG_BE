b7281224431f9532a267d4246d986f57e78fce4d
khanhnkq <khanhnkq.24itb@vku.udn.vn>
2026-02-01 15:17:28 +0700

    reaction to post

---
+++ src/main/java/org/example/connectcg_be/dto/ReactionRequest.java
@@ -0,0 +1,14 @@
+package org.example.connectcg_be.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReactionRequest {
+    private String reaction;
+}
