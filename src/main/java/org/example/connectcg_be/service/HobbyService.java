0945dd3820a31e47a3d8b01f2ff8ddb0168a0ee5
HoangLeoO <leoo09012001@gmail.com>
2026-02-02 10:41:24 +0700

    update sidebar và userNabar

--- src/main/java/org/example/connectcg_be/service/HobbyService.java
+++ src/main/java/org/example/connectcg_be/service/HobbyService.java
@@ -1,4 +1,8 @@
 package org.example.connectcg_be.service;
 
+import org.example.connectcg_be.dto.HobbyDTO;
+import java.util.List;
+
 public interface HobbyService {
+    List<HobbyDTO> getAllHobbies();
 }
--- src/main/java/org/example/connectcg_be/service/impl/HobbyServiceImpl.java
+++ src/main/java/org/example/connectcg_be/service/impl/HobbyServiceImpl.java
@@ -1,13 +1,30 @@
 package org.example.connectcg_be.service.impl;
 
+import org.example.connectcg_be.dto.HobbyDTO;
 import org.example.connectcg_be.repository.HobbyRepository;
 import org.example.connectcg_be.service.HobbyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class HobbyServiceImpl implements HobbyService {
 
     @Autowired
     private HobbyRepository hobbyRepository;
+
+    @Override
+    public List<HobbyDTO> getAllHobbies() {
+        return hobbyRepository.findAll().stream()
+                .map(hobby ->HobbyDTO.builder()
+                        .id(hobby.getId())
+                        .code(hobby.getCode())
+                        .name(hobby.getName())
+                        .icon(hobby.getIcon())
+                        .category(hobby.getCategory())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
