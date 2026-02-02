ac891aacd75f85aa03647e183f95922cdbddc9f5
HoangLeoO <leoo09012001@gmail.com>
2026-02-02 11:00:11 +0700

    oske

--- src/main/resources/db/migration/V17__add_client_cleared_at_to_chat_room_members.sql
+++ src/main/resources/db/migration/V17__add_client_cleared_at_to_chat_room_members.sql
@@ -1,2 +1,19 @@
-ALTER TABLE chat_room_members
-ADD COLUMN client_cleared_at DATETIME DEFAULT NULL;
+DROP PROCEDURE IF EXISTS upgrade_database_v17;
+
+DELIMITER $$
+CREATE PROCEDURE upgrade_database_v17()
+BEGIN
+    IF NOT EXISTS(
+        SELECT *
+        FROM information_schema.COLUMNS
+        WHERE TABLE_SCHEMA = DATABASE()
+        AND TABLE_NAME = 'chat_room_members'
+        AND COLUMN_NAME = 'client_cleared_at'
+    ) THEN
+        ALTER TABLE chat_room_members ADD COLUMN client_cleared_at DATETIME DEFAULT NULL;
+    END IF;
+END $$
+DELIMITER ;
+
+CALL upgrade_database_v17();
+DROP PROCEDURE upgrade_database_v17;
