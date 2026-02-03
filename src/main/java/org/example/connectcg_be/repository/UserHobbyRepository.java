d6f9683f00813b606ee344df5e0ea6446e9b5826
khanhnkq <khanhnkq.24itb@vku.udn.vn>
2026-02-03 14:57:48 +0700

    ok

--- src/main/java/org/example/connectcg_be/repository/UserHobbyRepository.java
+++ src/main/java/org/example/connectcg_be/repository/UserHobbyRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface UserHobbyRepository extends JpaRepository<UserHobby, UserHobbyId> {
     List<UserHobby> findByUserId(Integer userId);
+
+    void deleteByUserId(Integer userId);
 }
--- src/main/java/org/example/connectcg_be/service/UserHobbyService.java
+++ src/main/java/org/example/connectcg_be/service/UserHobbyService.java
@@ -1,4 +1,5 @@
 package org.example.connectcg_be.service;
 
 public interface UserHobbyService {
+    void updateUserHobbies(Integer userId, java.util.List<Integer> hobbyIds);
 }
